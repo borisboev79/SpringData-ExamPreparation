3daea6412bf003be26e88e7ac76e6b5f38cf3fa5
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-11-27 17:06:09 +0200

    Delete 03. Exam - 02 April 2022/RealEstateAgency directory

--- 03. Exam - 02 April 2022/RealEstateAgency/src/main/java/softuni/exam/util/constants/ApartmentType.java
+++
@@ -1,7 +0,0 @@
-package softuni.exam.util.constants;
-
-public enum ApartmentType {
-    two_rooms,
-    three_rooms,
-    four_rooms;
-}
--- 03. Exam - 02 April 2022/RealEstateAgency/src/main/java/softuni/exam/util/constants/Paths.java
+++
@@ -1,9 +0,0 @@
-package softuni.exam.util.constants;
-
-public enum Paths {
-    ;
-    public static final String TOWNS_JSON_PATH = "src/main/resources/files/json/towns.json";
-    public static final String AGENTS_JSON_PATH = "src/main/resources/files/json/agents.json";
-    public static final String APARTMENTS_XML_PATH = "src/main/resources/files/xml/apartments.xml";
-    public static final String OFFERS_XML_PATH = "src/main/resources/files/xml/offers.xml";
-}
